1fa10f68874b204e9f741bd3fef7c2b381bab033
srimathi30 <srimathiravi03@gmail.com>
2020-06-18 15:16:14 +0530

    make it better

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,7 +19,6 @@ import { SpeakerTermsComponent } from './speaker-terms/speaker-terms.component';
     WebinarInfoComponent,
     SessionOutlineComponent,
     SpeakerTermsComponent
-   
   ],
   imports: [
     BrowserModule,
