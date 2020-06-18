f7d6130955a897582002f2ba59d2167a8c490753
srimathi30 <srimathiravi03@gmail.com>
2020-06-18 16:21:05 +0530

    changed server.js

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const app = express();
  
 // Serve only the static files form the dist directory
 // Replace the '/dist/<to_your_project_name>'
-app.use(express.static(path.join(__dirname, '/dist/webinarForm')));
+app.use(express.static(path.join(__dirname+ '/dist')));
  
 app.get('*', function(req,res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
-  res.sendFile(path.join(__dirname, '/dist/webinarForm/index.html'));
+  res.sendFile(path.join(__dirname+ '/dist/webinarForm/index.html'));
 });
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
